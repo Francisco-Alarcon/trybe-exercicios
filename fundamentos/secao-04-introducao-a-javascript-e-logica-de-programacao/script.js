cd14a86951890d720b788de511e3176e42effc57
Francisco <fernandoouleticia@gmail.com>
2022-10-25 19:40:56 -0300

    exercicios secao 04-dia-4.3

---
+++ fundamentos/secao-04-introducao-a-javascript-e-logica-de-programacao/script.js
@@ -0,0 +1,26 @@
+//let fatorial = 1;
+
+//for (let index = 10; index > 0; index -= 1) {
+ // fatorial *= index;
+//}
+
+//console.log(fatorial);
+
+//let word = 'tryber';
+//let reverseWord = '';
+
+//for (let index = 0; index < word.length; index += 1) //{
+ // reverseWord += word[word.length - 1 - index];
+//}
+
+//console.log(reverseWord);
+
+//let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+//let menuServices = 'Serviços';
+
+//console.log(menuServices);
+
+//let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+//let indexOfPortfolio = menu.indexOf('Portfólio');
+
+//console.log(indexOfPortfolio);
